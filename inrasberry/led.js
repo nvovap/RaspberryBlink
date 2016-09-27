412ab80dad0a97bcee29ed6df1ac0bef2186a926
Vladimir Nevinniy <nvovap@gmail.com>
2016-09-27 17:34:12 +0300

    fff

---
+++ inrasberry/led.js
@@ -0,0 +1,89 @@
+var socket = require('socket.io-client')('http://10.10.1.56:3141');
+//var socket = require('socket.io-client')('http://192.168.2.1:3141');
+var gpio = require('rpi-gpio');
+//var fs = require('fs');
+
+// hack due to error
+//fs.exists = require('path').exists;
+//var async = require('async');
+
+// pin GPIO4
+var pin = 7;
+
+// current fps
+var piFps = 0;
+var currentValue = false;
+var timemanager;
+
+
+var set0 = function(err, results) {
+    if (err)
+        console.log(err);
+    console.log('Pin ' + pin + ' closed');
+    directWrite(pin, false, function() {
+        clearTimeout(timemanager);
+    });
+};
+
+var stopblink = true;
+
+var blinkexec = function() {
+    delayedWrite(7, true, function() {
+        delayedWrite(7, false, blinkexec)
+    });
+};
+
+var blink = function(err, results) {
+    if (err)
+        console.log(err);
+    console.log('Pin ' + pin + ' blinking');
+    blinkexec();
+};
+
+
+function directWrite(pin, value, callback) {
+    return gpio.write(pin, value, callback);
+
+}
+function delayedWrite(pin, value, callback) {
+    var delay = Math.round(1000 / piFps / 2);
+    
+    clearTimeout(timemanager);
+    timemanager = setTimeout(function() {
+        directWrite(pin, value, callback);
+    }, delay);
+}
+
+
+var release = function() {
+    console.log('Writes complete, pause then unexport pins');
+    setTimeout(function() {
+        gpio.destroy(function() {
+            console.log('Closed pins, now exit');
+            return process.exit(0);
+        });
+    }, 500);
+};
+
+
+socket.on('setfps', function(data) {
+        console.log(data);
+        
+        if (data.fps > 0) {
+            piFps = data.fps;
+            gpio.setup(pin, gpio.DIR_OUT, blink);
+        } else {
+            gpio.setup(pin, gpio.DIR_OUT, set0);
+        }
+    });
+    
+    socket.on('disconnect', function() {
+        console.log('disconnect');
+        release();
+    });
+
+socket.on('connect', function() {
+    console.log('connected');
+    
+    
+});
---
+++ main.js
@@ -0,0 +1,63 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var allClients = [];
+var count = 0;
+var fpsPi = 0;
+
+server.listen(3141);
+
+
+//app.use(express.static(__dirname+'/public')); 
+
+server.on('error', function(e) {
+    if (e.code == 'EADDRINUSE') {
+        console.log('Address in use, exit...');
+        process.exit();
+    }
+});
+
+app.get('/', function (req, res) {
+
+    res.sendfile(__dirname + '/main.html');
+
+  // res.send('Fps is ' + fpsPi);
+  // console.log('requested / - show ' + fpsPi);
+});
+
+function getDate() {
+    var datas = new Date();
+    return datas.getHours() + ':' + datas.getMinutes() + ':' + datas.getSeconds() + '.' + datas.getMilliseconds()
+}
+
+function consolelog(msg) {
+    console.log(getDate() + ' ' + msg);
+}
+
+io.on('connection', function (socket) {
+    io.emit('setfps', {fps: fpsPi});
+    
+    // browser subscribes to listen to the station 
+    socket.on('subscribe', function(data) {
+        socket.json.emit('subscribed', {fps: fpsPi});
+        io.emit('setfps', {fps: fpsPi});
+    });
+    
+    // disconnect on error. Browser will reconnect
+    socket.on('error', function() {
+        socket.disconnect();
+    });
+    
+    // client disconnects
+    socket.on('disconnect', function() {
+        consolelog('Client disconnected.');
+    });
+    
+    // save to RPi and browsers new fps
+    socket.on('setfps', function(fps){
+        fpsPi = fps;
+        consolelog('setfps ' + fpsPi);
+        io.emit('setfps', {fps: fpsPi});
+    });
+});
